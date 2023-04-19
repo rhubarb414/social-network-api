2279d19db6598e139946d552f4a77a26b3f1085b
rhubarb414 <ctophr.dubois@gmail.com>
2023-04-18 20:37:29 -0700

    Adds userController

---
+++ controllers/userController.js
@@ -0,0 +1,25 @@
+const { User } = require("../models");
+
+module.exports = {
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  getUserById(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: `User ${req.params.userId} not found` })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  createUser(req, res) {
+    User.create(req.body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(500).json(err));
+  },
+};
--- models/index.js
+++ models/index.js
@@ -1,5 +1,3 @@
 const User = require("./User");
 const Thought = require("./Thought");
-const Reaction = require("./Reaction");
-
-module.exports = { Student, Course };
+module.exports = { User, Thought };
